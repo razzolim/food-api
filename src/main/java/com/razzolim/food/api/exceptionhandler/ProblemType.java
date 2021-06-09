f095eca219d6b9f651ce31ff94da57f8fd939a37
Renan Azzolim <azzolimrenan2@gmail.com>
2021-06-09 20:34:53 -0300

    Iniciando implementação Bean Validation

--- src/main/java/com/razzolim/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/razzolim/food/api/exceptionhandler/ProblemType.java
@@ -21,6 +21,7 @@ import lombok.Getter;
 public enum ProblemType {
 
     PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+    DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos."),
     ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível."),
     RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
