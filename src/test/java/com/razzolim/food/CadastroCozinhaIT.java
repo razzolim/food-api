1e69e12fa62ff84c8e7a03ca0d753b1aa3062f32
Renan Azzolim <azzolimrenan2@gmail.com>
2021-06-18 16:50:38 -0300

    execução de teste com callback do Flyway
    Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/razzolim/food/CadastroCozinhaIT.java
+++ src/test/java/com/razzolim/food/CadastroCozinhaIT.java
@@ -1,9 +1,14 @@
 package com.razzolim.food;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -19,8 +24,31 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
     
+    @Autowired
+    private Flyway flyway;
+    
+    @Before
+    public void setUp() {
+	RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	RestAssured.port = port;
+	RestAssured.basePath = "/cozinhas";
+	
+	flyway.migrate();
+    }
+    
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+	
+	given()
+		.accept(ContentType.JSON)
+	.when()
+		.get()
+	.then()
+		.statusCode(HttpStatus.OK.value());
+    }
+    
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
 	RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 	
 	given()
@@ -30,7 +58,20 @@ public class CadastroCozinhaIT {
 	.when()
 		.get()
 	.then()
-		.statusCode(HttpStatus.OK.value());
+		.body("", hasSize(4))
+		.body("nome", hasItems("Indiana", "Tailandesa"));
+    }
+    
+    @Test
+    public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+	given()
+		.body("{ \"nome\": \"Chinesa\" }")
+		.contentType(ContentType.JSON)
+		.accept(ContentType.JSON)
+	.when()
+		.post()
+	.then()
+		.statusCode(HttpStatus.CREATED.value());
     }
 
 }
