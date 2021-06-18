529e422be6bdcff37f2f0f70ca5b3a8a2e2c8c2b
Renan Azzolim <azzolimrenan2@gmail.com>
2021-06-18 20:14:10 -0300

    escopo das transações

--- src/main/java/com/razzolim/food/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/razzolim/food/domain/service/CadastroCozinhaService.java
@@ -1,5 +1,7 @@
 package com.razzolim.food.domain.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -18,10 +20,12 @@ public class CadastroCozinhaService {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Transactional
     public Cozinha salvar(Cozinha cozinha) {
 	return cozinhaRepository.save(cozinha);
     }
 
+    @Transactional
     public void excluir(Long id) {
 	try {
 	    cozinhaRepository.deleteById(id);
