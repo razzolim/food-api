7b7bab64e0ef1c905cce68ccd3cfadf93e078ac2
Renan Azzolim <azzolimrenan2@gmail.com>
2021-07-24 00:58:00 -0300

    serviço de armazenagem de fotos no disco local

---
+++ src/main/java/com/razzolim/food/domain/service/FotoStorageService.java
@@ -0,0 +1,34 @@
+/*
+ * COPYRIGHT RENAN AZZOLIM 2021 - ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of Renan Azzolim.
+ */
+package com.razzolim.food.domain.service;
+
+import java.io.InputStream;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @author Renan Azzolim
+ *
+ * @since 
+ * 
+ */
+public interface FotoStorageService {
+    
+    void armazenar(NovaFoto novaFoto);
+    
+    @Builder
+    @Getter
+    class NovaFoto {
+        private String nomeArquivo;
+        private InputStream inputStream;
+    }
+
+}
---
+++ src/main/java/com/razzolim/food/infrastructure/service/storage/LocalFotoStorageService.java
@@ -0,0 +1,51 @@
+/*
+ * COPYRIGHT RENAN AZZOLIM 2021 - ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of Renan Azzolim.
+ */
+package com.razzolim.food.infrastructure.service.storage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import com.razzolim.food.domain.service.FotoStorageService;
+
+/**
+ * @author Renan Azzolim
+ *
+ * @since
+ * 
+ */
+@Service
+public class LocalFotoStorageService implements FotoStorageService {
+
+    @Value("${food.storage.local.diretorio-fotos}")
+    private Path diretorioFotos;
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+
+        try {
+            Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
+
+            FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
+
+        } catch (IOException e) {
+            throw new StorageException("Não foi possível armazenar arquivo.", e);
+        }
+    }
+
+    private Path getArquivoPath(String nomeArquivo) {
+        return diretorioFotos.resolve(Path.of(nomeArquivo));
+    }
+
+}
