e33bf1e3884ef30649305b92880a3d5736766a33
Renan Azzolim <azzolimrenan2@gmail.com>
2021-07-26 19:42:48 -0300

    Servindo arquivos de fotos pela API

--- src/main/java/com/razzolim/food/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/razzolim/food/api/controller/RestauranteProdutoFotoController.java
@@ -10,11 +10,14 @@
 package com.razzolim.food.api.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,6 +32,7 @@ import com.razzolim.food.domain.model.FotoProduto;
 import com.razzolim.food.domain.model.Produto;
 import com.razzolim.food.domain.service.CadastroProdutoService;
 import com.razzolim.food.domain.service.CatalogoFotoProdutoService;
+import com.razzolim.food.domain.service.FotoStorageService;
 
 /**
  * @author Renan Azzolim
@@ -45,6 +49,9 @@ public class RestauranteProdutoFotoController {
 
     @Autowired
     private CadastroProdutoService cadastroProduto;
+    
+    @Autowired
+    private FotoStorageService fotoStorageService;
 
     @Autowired
     private FotoProdutoModelAssembler assembler;
@@ -69,11 +76,27 @@ public class RestauranteProdutoFotoController {
 
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public FotoProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
         FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
 
         return assembler.toModel(fotoProduto);
+    }
+    
+    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+        
+        try {
+            FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
+            
+            InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+            
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(new InputStreamResource(inputStream));
+        } catch (Exception error) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
