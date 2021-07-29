0a15a37454a2854ff7e8a10a0f0170c4b125a170
Renan Azzolim <azzolimrenan2@gmail.com>
2021-07-29 19:47:36 -0300

    Habilitando CORS globalmente no projeto da API

---
+++ src/main/java/com/razzolim/food/core/web/WebConfig.java
@@ -0,0 +1,34 @@
+/*
+ * COPYRIGHT RENAN AZZOLIM 2021 - ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of Renan Azzolim.
+ */
+package com.razzolim.food.core.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Renan Azzolim
+ *
+ * @since 
+ * 
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        
+        registry.addMapping("/**")
+            .allowedMethods("*");
+//            .allowedOrigins("*"); padrao é *... 
+        
+    }
+
+}
