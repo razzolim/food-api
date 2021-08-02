569462fa8922d289399928d8d705523f360de38a
Renan Azzolim <azzolimrenan2@gmail.com>
2021-08-02 20:15:17 -0300

    Gerando a definição OpenAPI em JSON com SpringFox

---
+++ src/main/java/com/razzolim/food/core/openapi/SpringFoxConfig.java
@@ -0,0 +1,38 @@
+/*
+ * COPYRIGHT RENAN AZZOLIM 2021 - ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of Renan Azzolim.
+ */
+package com.razzolim.food.core.openapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Renan Azzolim
+ *
+ * @since 
+ * 
+ */
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                    .apis(RequestHandlerSelectors.any())
+                .build();
+    }
+
+}
