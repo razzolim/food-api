8d021d3b8aaf62195ed16d8fb3003d95a5ec3517
Renan Azzolim <azzolimrenan2@gmail.com>
2021-08-02 20:33:54 -0300

    Swagger UI
    Gerando a documentação da API em HTML com Swagger UI e SpringFox

--- src/main/java/com/razzolim/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/razzolim/food/core/openapi/SpringFoxConfig.java
@@ -11,6 +11,8 @@ package com.razzolim.food.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -25,7 +27,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+public class SpringFoxConfig implements WebMvcConfigurer {
     
     @Bean
     public Docket apiDocket() {
@@ -33,6 +35,15 @@ public class SpringFoxConfig {
                 .select()
                     .apis(RequestHandlerSelectors.any())
                 .build();
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+        
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
