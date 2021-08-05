2a061881e288a88e9e0c1ee96f4b102b7e853f87
Renan Azzolim <azzolimrenan2@gmail.com>
2021-08-04 21:41:05 -0300

    modelo de representação de problema com códigos de status de erro

--- src/main/java/com/razzolim/food/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/razzolim/food/core/openapi/SpringFoxConfig.java
@@ -28,6 +28,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
@@ -71,6 +72,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                 .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .message("Erro interno do servidor")
+                .responseModel(new ModelRef("Problema"))
                 .build(),
                 new ResponseMessageBuilder()
                 .code(HttpStatus.NOT_ACCEPTABLE.value())
@@ -83,10 +85,12 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                     .code(HttpStatus.BAD_REQUEST.value())
                     .message("Requisição inválida (erro do cliente)")
+                    .responseModel(new ModelRef("Problema"))
                     .build(),
                 new ResponseMessageBuilder()
                     .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                     .message("Erro interno no servidor")
+                    .responseModel(new ModelRef("Problema"))
                     .build(),
                 new ResponseMessageBuilder()
                     .code(HttpStatus.NOT_ACCEPTABLE.value())
@@ -104,10 +108,12 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 new ResponseMessageBuilder()
                     .code(HttpStatus.BAD_REQUEST.value())
                     .message("Requisição inválida (erro do cliente)")
+                    .responseModel(new ModelRef("Problema"))
                     .build(),
                 new ResponseMessageBuilder()
                     .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                     .message("Erro interno no servidor")
+                    .responseModel(new ModelRef("Problema"))
                     .build()
             );
     }
