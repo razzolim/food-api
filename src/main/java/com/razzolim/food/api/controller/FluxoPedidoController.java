374e3572c3af5f674441227c9042220e33ee207a
Renan Azzolim <azzolimrenan2@gmail.com>
2021-08-07 22:58:22 -0300

    impl documentação de endpoints de fluxo de pedidos

--- src/main/java/com/razzolim/food/api/controller/FluxoPedidoController.java
+++ src/main/java/com/razzolim/food/api/controller/FluxoPedidoController.java
@@ -11,12 +11,14 @@ package com.razzolim.food.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.razzolim.food.api.openapi.controller.FluxoPedidoControllerOpenApi;
 import com.razzolim.food.domain.service.FluxoPedidoService;
 
 /**
@@ -26,8 +28,8 @@ import com.razzolim.food.domain.service.FluxoPedidoService;
  * 
  */
 @RestController
-@RequestMapping(value = "/pedidos/{codigoPedido}")
-public class FluxoPedidoController {
+@RequestMapping(path = "/pedidos/{codigoPedido}", produces = MediaType.APPLICATION_JSON_VALUE)
+public class FluxoPedidoController implements FluxoPedidoControllerOpenApi {
     
     @Autowired
     private FluxoPedidoService fluxoPedido;
---
+++ src/main/java/com/razzolim/food/api/openapi/controller/FluxoPedidoControllerOpenApi.java
@@ -0,0 +1,58 @@
+/*
+ * COPYRIGHT RENAN AZZOLIM 2021 - ALL RIGHTS RESERVED.
+ * 
+ * This software is only to be used for the purpose for which it has been
+ * provided. No part of it is to be reproduced, disassembled, transmitted,
+ * stored in a retrieval system nor translated in any human or computer
+ * language in any way or for any other purposes whatsoever without the prior
+ * written consent of Renan Azzolim.
+ */
+package com.razzolim.food.api.openapi.controller;
+
+import com.razzolim.food.api.exceptionhandler.Problem;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+/**
+ * @author Renan Azzolim
+ *
+ * @since 
+ * 
+ */
+@Api(tags = "Pedidos")
+public interface FluxoPedidoControllerOpenApi {
+
+    @ApiOperation("Confirmação de pedido")
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Pedido confirmado com sucesso"),
+        @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
+    })
+    void confirmar(
+            @ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", 
+                required = true)
+            String codigoPedido);
+
+    @ApiOperation("Cancelamento de pedido")
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Pedido cancelado com sucesso"),
+        @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
+    })
+    void cancelar(
+            @ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", 
+                required = true)
+            String codigoPedido);
+
+    @ApiOperation("Registrar entrega de pedido")
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Entrega de pedido registrada com sucesso"),
+        @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
+    })
+    void entregar(
+            @ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", 
+                required = true)
+            String codigoPedido);
+}
