d93cbb8bb829135aba9c62f6a513150d671921ca
Renan Azzolim <azzolimrenan2@gmail.com>
2021-08-30 20:53:05 -0300

    Corrigindo link de coleção de recurso de responsáveis por restaurante

--- src/main/java/com/razzolim/food/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/razzolim/food/api/controller/RestauranteUsuarioResponsavelController.java
@@ -9,6 +9,9 @@
  */
 package com.razzolim.food.api.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
@@ -47,7 +50,10 @@ public class RestauranteUsuarioResponsavelController implements RestauranteUsuar
 	public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
-		return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis());
+		return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis())
+				.removeLinks()
+				.add(linkTo(methodOn(RestauranteUsuarioResponsavelController.class)
+						.listar(restauranteId)).withSelfRel());
 	}
 
 	@DeleteMapping("/{usuarioId}")
