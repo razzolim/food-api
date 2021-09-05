3ec6ffcf8620d9c185e3119fde094852e29605f3
Renan Azzolim <azzolimrenan2@gmail.com>
2021-09-04 23:22:57 -0300

    Desligando uma versão da API

--- src/main/java/com/razzolim/food/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/razzolim/food/core/springfox/SpringFoxConfig.java
@@ -69,7 +69,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig implements WebMvcConfigurer {
     
-    @Bean
+    @Bean // para depreciar a documentação só remover o bean pra esse docket
     public Docket apiDocketV1() {
         var typeResolver = new TypeResolver();
         
--- src/main/java/com/razzolim/food/core/web/ApiRetirementHandler.java
+++ src/main/java/com/razzolim/food/core/web/ApiRetirementHandler.java
@@ -3,13 +3,14 @@ package com.razzolim.food.core.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Component
-public class ApiDeprecationHandler extends HandlerInterceptorAdapter {
+public class ApiRetirementHandler extends HandlerInterceptorAdapter {
 	
-	@Override
+	/*@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		if (request.getRequestURI().startsWith("/v1")) {
@@ -19,6 +20,16 @@ public class ApiDeprecationHandler extends HandlerInterceptorAdapter {
 		}
 		
 		return true;
+	}*/
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		if (request.getRequestURI().startsWith("/v1")) {
+			response.setStatus(HttpStatus.GONE.value());
+		}
+		
+		return false;
 	}
 	
 	
