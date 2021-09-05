cded41a35b9a34436f6be2103bbfd703e8204172
Renan Azzolim <azzolimrenan2@gmail.com>
2021-09-04 23:16:55 -0300

    Depreciando uma versão da API

--- src/main/java/com/razzolim/food/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/razzolim/food/core/springfox/SpringFoxConfig.java
@@ -190,8 +190,10 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     
     private ApiInfo apiInfoV1() {
         return new ApiInfoBuilder()
-                .title("Food API")
-                .description("API aberta para clientes e restaurantes")
+                .title("Food API (Depreciada)")
+                .description("API aberta para clientes e restaurantes.<br>"
+                		+ "<strong>Essa versão da API está depreciada e deixará de existir a partir de dd/MM/YYYY. "
+                		+ "Use a versão mais atual da API.</strong>")
                 .version("1")
                 .contact(new Contact("Renan", "renan.azzolim", "azzolimrenan2@gmail.com"))
                 .build();
