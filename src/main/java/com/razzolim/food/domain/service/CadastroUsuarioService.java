52e57223e77c98c15cebd6a73f19cd71236b5dc2
Renan Azzolim <azzolimrenan2@gmail.com>
2021-09-16 15:13:58 -0300

    23.15. refatorando serviços de usuários para usar BCrypt

--- src/main/java/com/razzolim/food/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/razzolim/food/domain/service/CadastroUsuarioService.java
@@ -12,6 +12,7 @@ package com.razzolim.food.domain.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,56 +31,61 @@ import com.razzolim.food.domain.repository.UsuarioRepository;
 @Service
 public class CadastroUsuarioService {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-    
-    @Autowired
-    private CadastroGrupoService cadastroGrupo;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
-    @Transactional
-    public Usuario salvar(Usuario usuario) {
-	usuarioRepository.detach(usuario);
-	
-	Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
-	
-	if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
-	    throw new NegocioException(
-		    String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
-	}
-	
-        return usuarioRepository.save(usuario);
-    }
+	@Autowired
+	private CadastroGrupoService cadastroGrupo;
 
-    @Transactional
-    public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
-	Usuario usuario = buscarOuFalhar(usuarioId);
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-	if (usuario.senhaNaoCoincideCom(senhaAtual)) {
-	    throw new NegocioException(
-		    "Senha atual informada não coincide com a senha do usuário.");
+	@Transactional
+	public Usuario salvar(Usuario usuario) {
+		usuarioRepository.detach(usuario);
+
+		Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+		if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
+			throw new NegocioException(
+					String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
+		}
+
+		if (usuario.isNovo()) {
+			usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		}
+
+		return usuarioRepository.save(usuario);
 	}
 
-	usuario.setSenha(novaSenha);
-    }
-    
-    @Transactional
-    public void desassociarGrupo(Long usuarioId, Long grupoId) {
-        Usuario usuario = buscarOuFalhar(usuarioId);
-        Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
-        
-        usuario.removerGrupo(grupo);
-    }
+	@Transactional
+	public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
+		Usuario usuario = buscarOuFalhar(usuarioId);
 
-    @Transactional
-    public void associarGrupo(Long usuarioId, Long grupoId) {
-        Usuario usuario = buscarOuFalhar(usuarioId);
-        Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
-        
-        usuario.adicionarGrupo(grupo);
-    }
+		if (!passwordEncoder.matches(senhaAtual, usuario.getSenha())) {
+			throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
+		}
 
-    public Usuario buscarOuFalhar(Long usuarioId) {
-	return usuarioRepository.findById(usuarioId)
-		.orElseThrow(() -> new UsuarioNaoEncontradoException(usuarioId));
-    }
+		usuario.setSenha(passwordEncoder.encode(novaSenha));
+	}
+
+	@Transactional
+	public void desassociarGrupo(Long usuarioId, Long grupoId) {
+		Usuario usuario = buscarOuFalhar(usuarioId);
+		Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
+
+		usuario.removerGrupo(grupo);
+	}
+
+	@Transactional
+	public void associarGrupo(Long usuarioId, Long grupoId) {
+		Usuario usuario = buscarOuFalhar(usuarioId);
+		Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
+
+		usuario.adicionarGrupo(grupo);
+	}
+
+	public Usuario buscarOuFalhar(Long usuarioId) {
+		return usuarioRepository.findById(usuarioId).orElseThrow(() -> new UsuarioNaoEncontradoException(usuarioId));
+	}
 }
