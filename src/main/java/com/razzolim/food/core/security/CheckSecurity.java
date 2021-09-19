942f7b50f43843e0e037b76366eb5a9c7150e57c
Renan Azzolim <azzolimrenan2@gmail.com>
2021-09-19 17:17:24 -0300

    23.38. Corrigindo lógica de restrição de acessos para Client Credentials Flow
    23.38. Corrigindo lógica de restrição de acessos para Client Credentials Flow

--- src/main/java/com/razzolim/food/core/security/CheckSecurity.java
+++ src/main/java/com/razzolim/food/core/security/CheckSecurity.java
@@ -49,14 +49,14 @@ public @interface CheckSecurity {
 
 	    @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
 	    @PostAuthorize("hasAuthority('CONSULTAR_PEDIDOS') or "
-	    		+ "@foodSecurity.getUsuarioId() == returnObject.cliente.id or "
+	    		+ "@foodSecurity.usuarioAutenticado(returnObject.cliente.id) or "
 	    		+ "@foodSecurity.gerenciaRestaurante(returnObject.restaurante.id)")
 	    @Retention(RUNTIME)
 	    @Target(METHOD)
 	    public @interface PodeBuscar { }
 	    
 	    @PreAuthorize("hasAuthority('SCOPE_READ') and (hasAuthority('CONSULTAR_PEDIDOS') or " 
-	    		+ "@algaSecurity.getUsuarioId() == #filtro.clienteId or"
+	    		+ "@algaSecurity.usuarioAutenticado(#filtro.clienteId) or"
 	    		+ "@algaSecurity.gerenciaRestaurante(#filtro.restauranteId))")
 	    @Retention(RUNTIME)
 	    @Target(METHOD)
@@ -126,7 +126,7 @@ public @interface CheckSecurity {
 	    public @interface PodeAlterarPropriaSenha { }
 	    
 	    @PreAuthorize("hasAuthority('SCOPE_WRITE') and (hasAuthority('EDITAR_USUARIOS_GRUPOS_PERMISSOES') or "
-	            + "@algaSecurity.getUsuarioId() == #usuarioId)")
+	            + "@algaSecurity.usuarioAutenticado(#usuarioId))")
 	    @Retention(RUNTIME)
 	    @Target(METHOD)
 	    public @interface PodeAlterarUsuario { }
--- src/main/java/com/razzolim/food/core/security/FoodSecurity.java
+++ src/main/java/com/razzolim/food/core/security/FoodSecurity.java
@@ -11,32 +11,36 @@ import com.razzolim.food.domain.repository.RestauranteRepository;
 
 @Component
 public class FoodSecurity {
-	
+
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	public Authentication getAuthentication() {
 		return SecurityContextHolder.getContext().getAuthentication();
 	}
-	
+
 	public Long getUsuarioId() {
 		Jwt jwt = (Jwt) getAuthentication().getPrincipal();
 		return jwt.getClaim("usuario_id");
 	}
-	
+
 	public boolean gerenciaRestaurante(Long restauranteId) {
-	    if (restauranteId == null) {
-	        return false;
-	    }
-	    
-	    return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
+		if (restauranteId == null) {
+			return false;
+		}
+
+		return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
+	}
+
+	public boolean gerenciaRestauranteDoPedido(String codigoPedido) {
+		return pedidoRepository.isPedidoGerenciadoPor(codigoPedido, getUsuarioId());
 	}
 	
-	public boolean gerenciaRestauranteDoPedido(String codigoPedido) {
-	    return pedidoRepository.isPedidoGerenciadoPor(codigoPedido, getUsuarioId());
-	} 
-
+	public boolean usuarioAutenticado(Long usuarioId) {
+		return getUsuarioId() != null && usuarioId != null
+				&& getUsuarioId().equals(usuarioId);
+	}
 }
